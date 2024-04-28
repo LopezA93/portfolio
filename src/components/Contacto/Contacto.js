89d7740894b5c205f46c222d7a6cbb69abff91e6
Agustin Lopez <92869667+LopezA93@users.noreply.github.com>
2024-04-28 20:54:33 +0200

    change cv

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,57 +1,75 @@
-import { useState, } from "react";
+import { useState } from "react";
 import { Grid, Container, Button, Modal } from "@mui/material";
 import "./style/Contacto.scss";
 
-
 const Contacto = () => {
-    const [openModal, setOpenModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
 
+  const [values, setValues]= useState({})
+  const handleChange = (e) => {
+    setValues({...values, 
+        [e.target.name]:e.target.values})
+  }
+  const addContacto = (e) => {
+    console.log(values)
+    e.preventDefault();
+    // setOpenModal(true);
+  };
+  const volverHome = () => {
+    window.location.replace("");
+  };
+  return (
+    <>
+      <Container id="contacto">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12}>
+            <h1> Contacto</h1>
+            <form name="contact"  className="form">
+              <input type="hidden" name="form-name" value="contact" />
+              <label>Nombre completo</label>
+              <input
+                type="text"
+                required
+                placeholder="Ingresa tu nombre"
+                name="name"
+                onChange={handleChange}
+              />
+              <label>Correo Electronico</label>
+              <input
+                type="email"
+                required
+                placeholder="Ingresa tu correo electronico"
+                onChange={handleChange}
+                name="email"
+              />
 
-    const addContacto = (e) => {
-        e.preventDefault()
-        setOpenModal(true)
-    }
-    const volverHome = () => {
+              <label>Ingresa su mensaje</label>
+              <textarea required placeholder="..." name="message" />
+              <Button onClick={addContacto} className="btnContacto" variant="contained">
+                Enviar
+              </Button>
+            </form>
+          </Grid>
+          {/* <Grid item xs={12} md={6} className="datos">
+            <h1> Mis Datos</h1>
+            <p>Email: tuemail@ejemplo.com</p>
+            <p>Teléfono: 123456789</p>
 
-        window.location.replace('')
-
-
-    }
-    return (
-        <>
-            <Container id="contacto">
-                <Grid>
-                    <h1> Contacto</h1>
-                </Grid>
-                <Grid>
-                    <form name="contact" method="post" className="form" >
-                        <input type="hidden" name="form-name" value="contact" />
-                        <label>Nombre completo</label>
-                        <input type="text" required placeholder="Ingresa tu nombre" name="name" />
-                        <label>Correo Electronico</label>
-                        <input type="email" required placeholder="Ingresa tu correo electronico" name="correo" />
-                        <label>Ingresa su mensaje</label>
-                        <textarea required placeholder="..." name="message" />
-                        <Button type="submit" className="btnContacto" variant="contained" >Enviar</Button>
-                    </form>
-                </Grid>
-
-
-
-
-            </Container>
-            <Modal onClose={() => setOpenModal(false)} open={openModal}>
-                <div className="envioForm">
-                    <h4>Muchas gracias por contactarme, me comunicare con usted a la brevedad.</h4>
-                    <Button className="btnVolver" onClick={volverHome}  >Volver</Button>
-                </div>
-
-
-
-            </Modal>
-
-        </>
-
-    )
-}
-export default Contacto
+          </Grid> */}
+        </Grid>
+      </Container>
+      <Modal onClose={() => setOpenModal(false)} open={openModal}>
+        <div className="envioForm">
+          <h4>
+            Muchas gracias por contactarme, me comunicare con usted a la
+            brevedad.
+          </h4>
+          <Button className="btnVolver" onClick={volverHome}>
+            Volver
+          </Button>
+        </div>
+      </Modal>
+    </>
+  );
+};
+export default Contacto;
--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -1,45 +1,68 @@
-import ImagenPerfil from "./img/fondo.jpg"
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import ArticleIcon from '@mui/icons-material/Article';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import "./style/presentacion.scss"
-
-
+import ImagenPerfil from "./img/fondo.jpg";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import ArticleIcon from "@mui/icons-material/Article";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import "./style/presentacion.scss";
 
 const Presentacion = () => {
+  return (
+    <Container id="presentacion" className="boxPresentacion">
+      <div className="boxImg">
+        <img src={ImagenPerfil} />
+      </div>
+      <div className="boxPresentacionInfo">
+        <p>
+          {" "}
+          <span className="nombre">
+            Hola, soy Agustin Lopez, un gusto!
+          </span>{" "}
+          <br />
+          Soy desarrollador FullStack Developer Javascript - React Js - Next Js
+          - Node Js - React Native.
+          <br />
+          Soy Abogado recibido en la Facultad Nacional de Cordoba - Argentina, y
+          hace un tiempo, decidí adentrarme en el mundo de la programación, y
+          desde entonces, cada día me fascina más este apasionante campo. Mi
+          viaje en la programación comenzó cuando tomé la decisión de emigrar a
+          España. <br/>
+          Desde que me sumergí en el mundo de la tecnología y
+          descubrí mi verdadera pasión. Sin embargo, mi primer contacto
+          profesional con la programación fue en una startup en Argentina, donde
+          comencé a desarrollar mis habilidades y conocimientos. A principios de
+          este año, tuve la oportunidad de dar un paso más en mi carrera al
+          unirme a un grupo de comunicación en España como Fullstack. Esta
+          experiencia internacional ha sido invaluable para mi crecimiento
+          profesional, permitiéndome adquirir una perspectiva global y trabajar
+          en proyectos diversos y desafiantes. <br/>Me considero un profesional
+          dedicado y responsable, siempre en busca de nuevas oportunidades para
+          crecer y mejorar. Aspiro a desplegar mis habilidades técnicas y
+          blandas dentro del sector de la tecnología de la información, con el
+          objetivo de seguir creciendo profesionalmente y contribuir al éxito de
+          los proyectos en los que participo. ¡Estoy emocionado por lo que el
+          futuro me depara en este apasionante viaje en el mundo de la
+          tecnología!
+        </p>
+      </div>
+      <div className="redes">
+        <a href="CV.pdf" target="_blank">
+          <ArticleIcon className="iconoWA" />
+        </a>
+        <a href="mailto:agustin.vd@hotmail.com" target="_blank">
+          <AlternateEmailIcon className="iconoMail" />
+        </a>
+        <a href="https://www.linkedin.com/in/lopeza93/" target="_blank">
+          <LinkedInIcon className="iconoLI" />
+        </a>
+        <a href="https://github.com/LopezA93/" target="_blank">
+          <GitHubIcon className="iconoGH" />
+        </a>
+      </div>
+    </Container>
+  );
+};
 
-
-    return (
-        <Container id="presentacion" className="boxPresentacion">
-            
-            <div className="boxImg">
-                <img src={ImagenPerfil}/>
-            </div>
-            <div className="boxPresentacionInfo">
-            <p> <span className="nombre">Hola, soy Agustin Lopez, un gusto!</span> <br/>
-            Soy desarrollador Frontend, y en curso de ser desarrollador Fullstack.<br/>
-            Tengo conocimientos en HTML - CSS - BOOTSTRAPP - SASS - JAVASCRIPT - REACT JS - MATERIAL UI - FIREBASE. 
-            
-            </p>
-            </div>
-            <div className="redes">
-                <a href="CV.pdf" target="_blank"><ArticleIcon className="iconoWA"/></a>
-                <a href="mailto:agustin.vd@hotmail.com" target="_blank"><AlternateEmailIcon className="iconoMail"/></a>
-                <a href="https://www.linkedin.com/in/lopeza93/" target="_blank"><LinkedInIcon className="iconoLI"/></a>
-                <a href="https://github.com/LopezA93/" target="_blank"><GitHubIcon className="iconoGH"/></a>
-
-
-            </div>
-
-            
-        </Container>
-
-    )
-
-}
-
-export default Presentacion
+export default Presentacion;
